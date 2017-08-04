976e60cd3e0d8888bae1d87f3e901c3d00bd0fb6
Unknown <garnerlogan65@gmail.com>
2017-08-04 18:23:50 -0400

    Added addition/subtraction capability
    uses switch to determine addition or subtraction

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -11,13 +11,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var buttonCount = 0
+    //var buttonCount = 0
     
     @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var topTextField: UITextField!
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    @IBOutlet weak var additionSwitch: UISwitch!
+    
     @IBAction func buttonTapped(_ sender: Any) {
         
-        buttonCount = buttonCount + 1
+        let addition = additionSwitch.isOn
+        
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+            
+        }
+        
+/*
+        buttonCount += 1
         
         print(buttonCount)
         
@@ -27,8 +47,8 @@ class ViewController: UIViewController {
             myLabel.text = "You hit it more than 15 times!"
         }
         
+*/
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
